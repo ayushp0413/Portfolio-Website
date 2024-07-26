3846afa3a5159245ad4da4e7da2c15c5c9d5cd8d
Ayush Patidar <118504997+ayushp0413@users.noreply.github.com>
2024-07-26 12:40:10 +0530

    Update resume.js
    resume updated

--- src/assets/data/resume.js
+++ src/assets/data/resume.js
@@ -2,7 +2,7 @@ import profileImg from "../images/profile.png"
 
 const resume = {
     profileImg : profileImg,
-    link : "https://drive.google.com/file/d/1tkfHVBwwaqrokmtLjitlmkjoajS6QOsf/view?usp=drive_link",
+    link : "https://drive.google.com/file/d/1KJp0rwo9pLqby1hUQ1ZHu8SjX0ONjc_h/view?usp=drive_link",
     about : "“I'm a 4th year student pursuing my Computer Science degree from Mahakal Institute of Technology,Ujjain. Vice President of college, Management and Marketing lead at Meta Developer Circle and Competitive Programming Co-Lead at GDSC. Hard working student seeking employment, bringing forth a motivated attitude and a variety of powerful skills.Committed to utilizing my skills to further the mission of a company.”"
 }
 export default resume 
